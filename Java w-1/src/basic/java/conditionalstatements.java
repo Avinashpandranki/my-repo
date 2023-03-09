bb0146999689d35ca19fb0400c0e9ff4ec326538
Avinash Pandranki <avinashofficial7286@gmail.com>
2023-03-09 21:32:57 +0530

    Files are added in Intellij idea

--- Java w-1/src/basic/java/conditionalstatements.java
+++ Java w-1/src/basic/java/conditionalstatements.java
@@ -11,5 +11,33 @@ public class conditionalstatements {
             System.out.println("Number is +ve");
             }
         System.out.println("End of program-");
+        int na=7;
+        switch(na)
+        {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Looking forward to weekend");
+
+        }
     }
 }
---
+++ methods.java
@@ -0,0 +1,6 @@
+public class methods {
+    public static void main(String[] args) {
+        int a =7,b=3;
+        public int sum (int a,int b)
+           }
+}
